0d96b5f2c185c9b4ad7d7fa80eeb1a22b11ff51b
TrungCo <87183686+TrungCo@users.noreply.github.com>
2025-10-14 23:27:25 +0700

    di chuyen 0.5

--- demo2/src/main/java/org/example/demo2/GameApplication.java
+++ demo2/src/main/java/org/example/demo2/GameApplication.java
@@ -6,58 +6,69 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class GameApplication extends Application {
-  private static final int HEIGHT = 800;
-  private static final int WIDTH = 800;
-  private GraphicsContext gc;
-  private Ball ball;
-  private Paddle paddle;
-  private Brick brick;
-  static Image background;
+    private static final int HEIGHT = 800;
+    private static final int WIDTH = 800;
+    private GraphicsContext gc;
+    private Ball ball;
+    private Paddle paddle;
+    static Image background;
 
-  @Override
-  public void start(Stage stage) {
-    Canvas canvas = new Canvas(WIDTH, HEIGHT);
-    gc = canvas.getGraphicsContext2D();
 
-    StackPane root = new StackPane(canvas);
-    Scene scene = new Scene(root, WIDTH, HEIGHT);
+    @Override
+    public void start(Stage stage) {
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        gc = canvas.getGraphicsContext2D();
 
-    stage.setTitle("Arkanoid");
-    stage.setScene(scene);
-    stage.show();
+        StackPane root = new StackPane(canvas);
+        Scene scene = new Scene(root,WIDTH,HEIGHT);
 
-    // khoi tao
-    ball = new Ball(50, 50);
-    paddle = new Paddle(319, 773);
-    brick = new Brick(100, 100, 680, 75);
-    background =
-        new Image(
-            Objects.requireNonNull(getClass().getResourceAsStream("/asset/images/background.jpg")));
+        stage.setTitle("Arkanoid");
+        stage.setScene(scene);
+        stage.show();
 
-    AnimationTimer gameLoop =
-        new AnimationTimer() {
-          @Override
-          public void handle(long now) {
-            update();
-            render();
-          }
+        //khoi tao
+        ball = new Ball(Config.ballX,Config.ballY);
+        paddle = new Paddle(Config.paddleX,Config.paddleY);
+        background = new Image(Objects.requireNonNull(
+                getClass().getResourceAsStream("/asset/images/background.jpg")));
+
+        //key sensor
+        scene.setOnKeyPressed(e->{
+            if (e.getCode() == KeyCode.A)  Config.leftPressed = true;
+            if (e.getCode() == KeyCode.D) Config.rightPressed = true;
+        });
+        scene.setOnKeyReleased(e -> {
+            if (e.getCode() == KeyCode.A)  Config.leftPressed = false;
+            if (e.getCode() == KeyCode.D) Config.rightPressed = false;
+        });
+
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                    update();
+                    render();
+            }
         };
-    gameLoop.start();
-  }
+        gameLoop.start();
 
-  private void update() {}
+    }
 
-  private void render() {
-    gc.clearRect(0, 0, WIDTH, HEIGHT);
-    gc.drawImage(background, 0, 0, WIDTH, HEIGHT);
-    ball.render(gc);
-    paddle.render(gc);
-    brick.render(gc);
-  }
+    private void update(){
+        ball.update();
+        paddle.update(Config.leftPressed,Config.rightPressed);
+    }
+    private void render(){
+        gc.clearRect(0, 0, WIDTH, HEIGHT);
+        gc.drawImage(background, 0, 0, WIDTH, HEIGHT);
+        ball.render(gc);
+        paddle.render(gc);
+
+    }
 }
