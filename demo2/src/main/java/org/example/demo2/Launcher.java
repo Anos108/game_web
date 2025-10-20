f3e8eec9c30296f58e62c67018374b3501df849d
LÊ HÀ SƠN <24022727@vnu.edu.vn>
2025-10-20 20:07:29 +0700

    change the comment

--- demo2/src/main/java/org/example/demo2/Launcher.java
+++ demo2/src/main/java/org/example/demo2/Launcher.java
@@ -8,24 +8,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException; // Nhớ import
 
-public class Launcher extends Application { // Tên class của ông có thể khác
+public class Launcher extends Application {
 
     // Vứt hết biến WIDTH, HEIGHT, gc... ở đây đi
 
     @Override
-    public void start(Stage primaryStage) { // Tên biến 'stage' hay 'primaryStage' đều được
+    public void start(Stage primaryStage) {
         try {
-            // 1. TÌM FILE FXML (Sửa lại đường dẫn nếu cần)
+            // find fxml file
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/demo2/MainMenu.fxml"));
 
-            // 2. LOAD NÓ
+            // load fxml file
             Parent root = loader.load(); // Đây là lúc MenuController được tạo
 
-            // 3. TẠO SCENE TỪ CÁI ROOT (GIAO DIỆN) VỪA LOAD
+            // create a scene
             Scene scene = new Scene(root);
 
 
-            // 4. HIỂN THỊ
+            // display the scene
             primaryStage.setTitle("Arkanoid"); // Sửa title cho đúng
             primaryStage.setScene(scene);
             primaryStage.show();
