7bb1ce77104055a276134915aca85a5c9b43d4de
vanthang <24022731@vnu.edu.vn>
2025-11-04 20:32:38 +0700

    thêm bricks

--- demo2/src/main/java/org/example/demo2/GameOverController.java
+++ demo2/src/main/java/org/example/demo2/GameOverController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import javafx.scene.control.Label;
